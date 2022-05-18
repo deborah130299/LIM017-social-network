4b2c1787a4c9d29c58b3f049a3260e166dfdc5ce
deborah130299 <debeframart@gmail.com>
2022-05-17 21:59:34 -0500

    borrar-post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,10 @@
 //import { cierreActividadUsuario } from "../firebase/funcionesAuth.js";
-/* import { onNavigate } from "../main.js"; */
 import {
   savePosts,
   onSnapshot,
   collection,
   db,
   deletePosts,
-
 } from "../lib/firebaseConfig.js";
 //conGetPosts, deletePosts
 // Renderizando el header
@@ -34,55 +32,48 @@ export const Post = () => {
         </form>
         <div id='all-posts'></div>
         `;
-  PostElement.innerHTML = containerPost;
+        PostElement.innerHTML = containerPost;
 
-  const createPost = PostElement.querySelector("#create-Post");
-  const postContainer = PostElement.querySelector("#all-posts");
-
-  window.addEventListener("DOMContentLoaded", async () => {
-    onSnapshot(collection(db, "Posts"), (querySnapshot) => {
-      let html = "";
-
-      querySnapshot.forEach((doc) => {
-        const task = doc.data();
-        html += `
-            <div class='post-public'>
-                <textarea class='post-public'>${task.description}</textarea>
-                <button class='btn-borrar' data-id='${doc.id}'>Borrar</button>
-            </div>
-            `;
-      });
-      postContainer.innerHTML = html;
-      const btnsDelete = postContainer.querySelectorAll(".btn-borrar");
-      console.log(btnsDelete);
-
-      btnsDelete.forEach((btn) => {
-        btn.addEventListener("click", (e) => {
-          console.log("aaaaaaa");
-          //e.preventDefault();
-          deletePosts(e.target.dataset.id)
-            .catch((err) => {
-              console.log(err);
+        const createPost = PostElement.querySelector("#create-Post");
+        const postContainer = PostElement.querySelector("#all-posts");
+      
+        window.addEventListener("DOMContentLoaded", async () => {
+          onSnapshot(collection(db, "Posts"), (querySnapshot) => {
+            let html = "";
+      
+            querySnapshot.forEach((doc) => {
+              const task = doc.data();
+              html += `
+                  <div class='post-public'>
+                      <textarea class='post-public'>${task.description}</textarea>
+                      <button class='btn-borrar' data-id='${doc.id}'>Borrar</button>
+                  </div>
+                  `;
             });
+            postContainer.innerHTML = html;
+            const btnsDelete = postContainer.querySelectorAll(".btn-borrar");
+            console.log(btnsDelete);
+      
+            btnsDelete.forEach((btn) => {
+              btn.addEventListener("click", (e) => {
+                console.log("aaaaaaa");
+                //e.preventDefault();
+                deletePosts(e.target.dataset.id)
+                  .catch((err) => {
+                    console.log(err);
+                  });
+              });
+            });
+          });
         });
-      });
-    });
-  });
-
-  createPost.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const description = document.getElementById("post-text");
-
-    savePosts(description.value);
-  });
-
-/*   PostElement.querySelector('#buttonLogout').addEventListener('click', () => {
-    signOff()
-      .then(() => onNavigate('/'))
-      .catch((error) => {
-        console.log('No pudo cerrar sesión', error);
-      });
-    }); */
-  return PostElement;
-};
+      
+        createPost.addEventListener("submit", (e) => {
+          e.preventDefault();
+      
+          const description = document.getElementById("post-text");
+      
+          savePosts(description.value);
+        });
+      
+        return PostElement;
+      };
