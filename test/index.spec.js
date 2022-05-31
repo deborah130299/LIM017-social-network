d0c2251b28273c1a614556ad69721eb709b5e405
cfape <c.penagos16@gmail.com>
2022-05-30 19:39:08 -0500

    iniciando test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,1 @@
-// importamos la funcion que vamos a testear
-import { Login } from '../src/components/Login.js';
 
-describe('Login', () => {
-  it('debería ser una función', () => {
-    expect(typeof Login).toBe('function');
-  });
-});
